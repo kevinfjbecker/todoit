9ecbb061c215b0d1b80a89217f4d41f784be1b7b
Kevin Becker <kevinfjbecker@gmail.com>
2024-12-07 00:44:42 +0100

    Update ui_playground.js

--- ui_playground.js
+++ ui_playground.js
@@ -1,6 +1,4 @@
 import { select, Separator } from '@inquirer/prompts';
-// Or
-// import select, { Separator } from '@inquirer/select';
 
 const state = {
     file: false,
@@ -14,66 +12,87 @@ const run = async ( ) =>
     while(answer !== 'exit')
     {
         answer = await doSelector()
+        actions[answer]()
     }
 }
 
+const actions = {
+    "clear": () => { console.clear() },
+    "delete": () => { console.log('deleted.') },
+    "exit": () => 
+    {
+        console.clear()
+        process.exit(0)
+    },
+    "fetch": () => { state.api = true },
+    "markdown": () => { console.log('marked down.')},
+    "parse": () => { state.markdown = true },
+    "push": () => { console.log('pushed.') },
+    "read": () => {state.file = true },
+    "write": () => { console.log('written.')}
+}
+
+const getChoices = () =>
+    [
+        {
+            "name": "clear",
+            "value": "clear",
+            "disabled": false,
+            "description": "clear the console"
+        },
+        {
+            "name": "delete",
+            "value": "delete",
+            "disabled": state.api ? false : "- run fetch first",
+            "description": "delete one or all project(s) and tasks"
+        },
+        {
+            "name": "exit",
+            "value": "exit",
+            "disabled": false,
+            "description": "quit application"
+        },
+        {
+            "name": "fetch",
+            "value": "fetch",
+            "disabled": false,
+            "description": "fetch all projects and tasks from todoist API"
+        },
+        {
+            "name": "markdown",
+            "value": "markdown",
+            "disabled": state.api || state.file ? false : "- run fetch|read first",
+            "description": "write a project to markdown"
+        },
+        {
+            "name": "parse",
+            "value": "parse",
+            "disabled": false,
+            "description": "import a project from markdown"
+        },
+        {
+            "name": "push",
+            "value": "push",
+            "disabled": state.markdown ? false : "- run parse first",
+            "description": "push a project and tasks to todoist API"
+        },
+        {
+            "name": "read",
+            "value": "read",
+            "disabled": false,
+            "description": "read raw projects and tasks from file"
+        },
+        {
+            "name": "write",
+            "value": "write",
+            "disabled": state.api ? false : "- run fetch|read|parse first",
+            "description": "write raw projects and tasks to file"
+        }
+    ]
+
 const doSelector = async ( ) => select({
   message: 'Select an action',
-  choices: [
-    {
-        "name": "clear",
-        "value": "clear",
-        "disabled": false,
-        "description": "clear the console"
-    },
-    {
-        "name": "delete",
-        "value": "delete",
-        "disabled": "- run fetch first",
-        "description": "delete one or all project(s) and tasks"
-    },
-    {
-        "name": "exit",
-        "value": "exit",
-        "disabled": false,
-        "description": "quit application"
-    },
-    {
-        "name": "fetch",
-        "value": "fetch",
-        "disabled": false,
-        "description": "fetch all projects and tasks from todoist API"
-    },
-    {
-        "name": "markdown",
-        "value": "markdown",
-        "disabled": "- run fetch|read first",
-        "description": "write a project to markdown"
-    },
-    {
-        "name": "parse",
-        "value": "parse",
-        "disabled": false,
-        "description": "import a project from markdown"
-    },
-    {
-        "name": "push",
-        "value": "push",
-        "disabled": "- run parse first",
-        "description": "push a project and tasks to todoist API"
-    },
-    {
-        "name": "read",
-        "value": "read",
-        "disabled": false,
-        "description": "read raw projects and tasks from file"
-    },
-    {
-        "name": "write",
-        "value": "write",
-        "disabled": "- run fetch|read|parse first",
-        "description": "write raw projects and tasks to file"
-    }
-]});
+  choices: getChoices()
+});
 
 run()
