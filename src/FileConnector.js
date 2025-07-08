46bcb8b7c7fb49f7f4308259882ec7e2044636bd
Kevin Becker <kevinfjbecker@gmail.com>
2025-07-08 13:24:25 +0200

    Update FileConnector.js

--- src/FileConnector.js
+++ src/FileConnector.js
@@ -24,4 +24,4 @@ export default class FileConnector
 const getDateString = (d) =>
     d.getFullYear() +
     ('' + (d.getMonth()+1)).padStart(2,'0') +
-    d.getDate()
+    ('' + d.getDate()).padStart(2, '0')
